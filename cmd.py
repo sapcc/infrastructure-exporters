d4d6ed3b571d4f62c660de3d017c44e5dbc9c22f
Shane Parker <sha.parker@sap.com>
2018-11-29 13:04:49 -0600

    Added concurrency

--- cmd.py
+++ cmd.py
@@ -1,8 +1,13 @@
 import time
 import argparse
 import os
+import exporter
+import asyncio
+from concurrent.futures import FIRST_COMPLETED
+from yamlconfig import YamlConfig
 from vc_exporters.vc_exporter_types import vcapiandversions, vccustomervmmetrics
 from apic_exporters.apic_exporter_types import apichealth
+
 
 EXPORTERS = {
     'vcapiandversions': vcapiandversions.Vcapiandversions,
@@ -17,28 +22,70 @@ class ExporterFactory(object):
             return EXPORTERS[exportertype.lower()](**kwargs)
         except Exception as ex:
             print(str(ex))
-            print("couldn't create object")
             return None
 
 if __name__ == "__main__":
 
+    exporterConfigMapping = {}
     # Take a list of exporter configs and runs their collectors and exporters
     parser = argparse.ArgumentParser(description='Arguments for vcexporter.py')
     parser.add_argument(
-        "-c", "--exporterconfig", help="Specify config file for the exporter", default=os.getcwd() + "/samples/vcconfig.yaml")
+        "-c", "--exporterconfigfiles", help="Specify config files for the exporters, comma separated, full path")
     parser.add_argument(
-        "-t", "--exportertype", help="Specify exporter type [vcapiandversions, vccustomervmmetrics, vccustomerdsmetrics]", default='vcapiandversions')
+        "-f", "--singleconfifigfile", help="Specify full path to single config file to run only one exporter.  Must use -t flag also to specify type." )
+    parser.add_argument(
+        "-t", "--exportertype", help="Specify exporter type [vcapiandversions, vccustomervmmetrics, vccustomerdsmetrics, apichealth] Used with -f flag")
     args, remaining_argv = parser.parse_known_args()
-    if not args.exporterconfig.startswith("/"):
-        print("Please specify full path to config file")
-        exit(0)
-
-    # create specified exporter type and run_loop
-    try:
-        runningExporter = ExporterFactory.create_exporter(args.exportertype, exporterType=args.exportertype, exporterConfig=args.exporterconfig)
-    except Exception as ex:
-        print(str(ex))
-    if runningExporter != None:
-        runningExporter.run_loop(runningExporter.duration)
+    
+    # Process arguments and create exporterConfigMapping
+    if args.exporterconfigfiles is not None:
+        configFiles = args.exporterconfigfiles.split(',')
+        for configFile in configFiles:
+            if not configFile.startswith("/"):
+                print("Please use full path to config files")
+                exit(0)
+            else:
+                exporterConfigs = exporter.Exporter.get_config(configFile)
+                for exporterType in exporterConfigs['exporter_types']:
+                    exporter_enabled = exporterConfigs.get('exporter_types', {}).get(exporterType, {}).get('enabled')
+                    if exporter_enabled:
+                        exporterConfigMapping[exporterType] = configFile
+    
+    elif args.singleconfifigfile is not None and args.exportertype is not None:
+        if not args.singleconfifigfile.startswith("/"):
+            print("Please use full path to configfile")
+            exit(0)
+        else:
+            exporterConfigMapping[args.exportertype] = args.singleconfifigfile
     else:
-        print("Couldn't create exporter type: " + args.exportertype)
+        parser.print_help()
+
+    # Create an asynchronous exporter for each exporter in the exporterConfigMapping
+    run_loops = []
+    for exportertype in exporterConfigMapping:
+        currentExporter = ExporterFactory.create_exporter(exportertype, exporterType=exportertype, exporterConfig=exporterConfigMapping[exportertype])
+        if currentExporter != None:
+            run_loops.append((currentExporter.run_loop, currentExporter.duration))
+        else:
+            print("Couldn't add exporter "  + exportertype)
+
+    task_map = {}
+    for task in run_loops:
+        task_map[task] = asyncio.async(task[0](task[1]))
+    loop = asyncio.get_event_loop()
+    loop.run_forever()
+
+
+    # if not args.exporterconfig.startswith("/"):
+    #     print("Please specify full path to config file")
+    #     exit(0)
+
+    # # create specified exporter type and run_loop
+    # try:
+    #     runningExporter = ExporterFactory.create_exporter(args.exportertype, exporterType=args.exportertype, exporterConfig=args.exporterconfig)
+    # except Exception as ex:
+    #     print(str(ex))
+    # if runningExporter != None:
+    #     runningExporter.run_loop(runningExporter.duration)
+    # else:
+    #     print("Couldn't create exporter type: " + args.exportertype)
--- exporter.py
+++ exporter.py
@@ -1,11 +1,11 @@
 import os
 import sys
-from abc import ABC, abstractmethod
-from yamlconfig import YamlConfig
 import logging
 import time
+import asyncio
 from datetime import timedelta, datetime
-
+from abc import ABC, abstractmethod
+from yamlconfig import YamlConfig
 
 class Exporter(ABC):
 
@@ -30,27 +30,38 @@ class Exporter(ABC):
     def export(self):
         pass
 
+    @asyncio.coroutine
     def run_loop(self, duration):
          # Start infinite loop to get metrics
         while True:
             logging.debug('====> total loop start: %s' % datetime.now())
             # get the start time of the loop to be able to fill it to intervall exactly at the end
-            loop_start_time = int(time.time())
-
+            
+            yield None
+            collect_start_time = int(time.time())
             self.collect()
+            collect_end_time = int(time.time())
+   
+            yield None
+            export_start_time = int(time.time())
             self.export()
+            export_end_time = int(time.time())
 
-            loop_end_time = int(time.time())
+            total_loop_time = ((collect_end_time - collect_start_time) + (export_end_time - export_start_time))
+            #loop_end_time = int(time.time())
             logging.info('number of ' + self.exporterType + ' we got metrics for ' +
-                         str(self.metric_count) + " " + self.exporterType +
-                         '\'s - actual runtime: ' + str(loop_end_time - loop_start_time) + 's')
-
+                        str(self.metric_count) + " " + self.exporterType +
+                        '\'s - actual runtime: ' + str(total_loop_time) + 's')
+                        #'\'s - actual runtime: ' + str(loop_end_time - loop_start_time) + 's')
+                         
             # this is the time we sleep to fill the loop runtime until it reaches "interval"
             # the 0.9 makes sure we have some overlap to the last interval to avoid gaps in
             # metrics coverage (i.e. we get the metrics quicker than the averaging time)
             loop_sleep_time = 0.9 * \
                 self.exporterConfig['exporter_types'][self.exporterType]['collection_interval'] - \
-                (loop_end_time - loop_start_time)
+                (collect_end_time - collect_start_time) + (export_end_time - export_end_time)
+                #(loop_end_time - loop_start_time)
+                
             if loop_sleep_time < 0:
                 logging.warn('getting the metrics takes around ' + str(
                     self.exporterConfig['exporter_types'][self.exporterType]['collection_interval']) + ' seconds or longer - please increase the interval setting')
@@ -69,3 +80,6 @@ class Exporter(ABC):
             except IOError as e:
                 print("Couldn't open configuration file: " + str(e))
             return config
+        else:
+            logging.error("Config file doesn't exist: " + configurationfile)
+            exit(0)
