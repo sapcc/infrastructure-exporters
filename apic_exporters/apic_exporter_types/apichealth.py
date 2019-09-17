d3a635d2ca3018b4bffebd0ba054eee450088d75
i81196 <sha.parker@sap.com>
2019-09-17 07:34:20 -0500

    [apic-exporters] Added check for duplicate IPs

--- apic_exporters/apic_exporter_types/apichealth.py
+++ apic_exporters/apic_exporter_types/apichealth.py
@@ -1,3 +1,4 @@
+import re
 from apic_exporters.apic_exporter import Apicexporter
 from prometheus_client import Gauge
 
@@ -17,12 +18,17 @@ class Apichealth(Apicexporter):
                                                           ['hostname'])
         self.gauge['network_apic_physcial_interface_resets'] = Gauge('network_apic_physcial_interface_resets',
                                         'network_apic_physcial_interface_resets',
-                                        ['interfaceID'])                                               
+                                        ['interfaceID'])  
+        self.gauge['network_apic_duplicate_ip'] = Gauge('network_apic_duplicate_ip',
+                                                         'network_apic_duplicate_ip',
+                                                         ['apic_host',
+                                                         'ip', 'mac', 'node_id'])
                                     
     def collect(self):
         self.metric_count = 0
         for apicHost in self.apicHosts:
             self.apicHosts[apicHost]['physIf'] = []
+            self.apicHosts[apicHost]['duplicateIps'] = []
             if self.apicHosts[apicHost]['canConnectToAPIC'] == False:
                 continue
 
@@ -40,12 +46,23 @@ class Apichealth(Apicexporter):
                 self.metric_count += 3
             
             physIfUrl = "https://" + self.apicHosts[apicHost]['name'] + "/api/node/class/ethpmPhysIf.json?"
-            physIfInfo = apicPodInfo = self.apicGetRequest(physIfUrl, self.apicHosts[apicHost]['loginCookie'], self.apicInfo['proxy'],apicHost)
+            physIfInfo = self.apicGetRequest(physIfUrl, self.apicHosts[apicHost]['loginCookie'], self.apicInfo['proxy'],apicHost)
             if self.apicHosts[apicHost]['status_code'] == 200 and physIfInfo != None:
                 for physIf in physIfInfo['imdata']:
                     resetCtr = physIf['ethpmPhysIf']['attributes']['resetCtr']
                     physIfDN = physIf['ethpmPhysIf']['attributes']['dn']
                     self.apicHosts[apicHost]['physIf'].append({'dn':physIfDN, 'resetCtr':resetCtr, 'status_code':200})
+                    self.metric_count += 1
+
+            duplicateIpsUrl = 'https://' + self.apicHosts[apicHost]['name'] + '/api/node/class/fvIp.json?rsp-subtree=full&'
+            duplicateIpsUrl += 'rsp-subtree-class=fvReportingNode&query-target-filter=and(ne(fvIp.debugMACMessage,""))'
+            duplicateIpsInfo = self.apicGetRequest(duplicateIpsUrl, self.apicHosts[apicHost]['loginCookie'], self.apicInfo['proxy'],apicHost)
+            if self.apicHosts[apicHost]['status_code'] == 200 and duplicateIpsInfo != None and int(duplicateIpsInfo['totalCount']) > 0:
+                for duplicateIp in duplicateIpsInfo['imdata']:
+                    ipAddres = duplicateIp['fvIp']['attributes']['addr']
+                    ipMac = re.search(r"([0-9A-F]{2}:){5}[0-9A-F]{2}", duplicateIp['fvIp']['attributes']['dn']).group()
+                    ipNode = duplicateIp['fvIp']['children'][0]['fvReportingNode']['attributes']['id']
+                    self.apicHosts[apicHost]['duplicateIps'].append({'ip':ipAddres, 'mac':ipMac, 'apicNode':ipNode, 'status_code':200})
                     self.metric_count += 1
 
     def export(self):
@@ -67,3 +84,14 @@ class Apichealth(Apicexporter):
                     self.gauge['network_apic_physcial_interface_resets'].labels(physIfLabel).set(physIf['resetCtr'])       
                 else:
                     self.gauge['network_apic_physcial_interface_resets'].labels(physIfLabel).set(-1)
+
+            for duplicateIp in self.apicHosts[apicHost]['duplicateIps']:
+                if duplicateIp['status_code'] == 200:
+                    self.gauge['network_apic_duplicate_ip'].labels(self.apicHosts[apicHost]['name'],
+                                                                   duplicateIp['ip'],
+                                                                   duplicateIp['mac'],
+                                                                   duplicateIp['apicNode']).set(1)
+                else:
+                    self.gauge['network_apic_duplicate_ip'].labels(self.apicHosts[apicHost]['name'],ip,mac,node).set(-1)
+
+                    
