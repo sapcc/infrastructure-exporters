c9078311d173a5c9566604260e5af4ddd69c401d
Tommy Sauer <tommy.sauer@sap.com>
2019-10-23 11:10:22 +0200

    total amount of clusters, adding failover_host to metric

--- vc_exporters/vc_exporter_types/vcapiandversions.py
+++ vc_exporters/vc_exporter_types/vcapiandversions.py
@@ -30,16 +30,19 @@ class Vcapiandversions(VCExporter):
                                                                 'vcenter_vcenter_api_active_count',
                                                                 ['hostname'])
 
-        self.gauge['vcenter_vcenter_vms_on_failover_hosts'] = Gauge('venter_vcenter_vms_on_failover_hosts',
+        self.gauge['vcenter_vms_on_failover_hosts'] = Gauge('vcenter_vms_on_failover_hosts',
                                                                 'Count of VMs placed on failover hosts, they shouldn\'t be there',
-                                                                ['hostname','cluster'])
+                                                                ['hostname','cluster','failover_host'])
 
-        self.gauge['vcenter_vcenter_failover_host'] = Gauge('vcenter_vcenter_failover_host',
+        self.gauge['vcenter_failover_host'] = Gauge('vcenter_failover_host',
                                                         'Count of failover hosts in prod clusters',
                                                         ['hostname','cluster'])
 
+        self.gauge['vcenter_prod_cluster'] = Gauge('vcenter_prod_cluster',
+                                                    'Count of prod cluster in a vcenter',
+                                                    ['hostname'])
+
         self.content = self.si.RetrieveContent()
-        #never used?
         self.clusters = [cluster for cluster in
                          self.content.viewManager.CreateContainerView(
                              self.content.rootFolder, [vim.ComputeResource],
@@ -96,28 +99,37 @@ class Vcapiandversions(VCExporter):
 
 
         collected_spare_hosts = dict()
+        cluster_count = 0
         for cluster in self.clusters:
             if "prod" in cluster.name:
-                if cluster.configuration.dasConfig.admissionControlEnabled \
-                    and cluster.configuration.dasConfig.admissionControlPolicy.failoverLevel == 1 \
-                    and len(cluster.configuration.dasConfig.admissionControlPolicy.failoverHosts) == 1:
-                    for host in cluster.configuration.dasConfig.admissionControlPolicy.failoverHosts:
-                        if cluster.name in collected_spare_hosts.keys():
-                            #add another element if we have this cluster and more than one spare
-                            collected_spare_hosts[cluster.name].append({'name' : host.name, 'vms' : len(host.vm)})
-                        else:
-                            collected_spare_hosts[cluster.name] = [{ 'name': host.name, 'vms': len(host.vm)}]
+                cluster_count += 1
+                try:
+                    if cluster.configuration.dasConfig.admissionControlEnabled \
+                        and cluster.configuration.dasConfig.admissionControlPolicy.failoverLevel == 1 \
+                        and len(cluster.configuration.dasConfig.admissionControlPolicy.failoverHosts) == 1:
+                        for host in cluster.configuration.dasConfig.admissionControlPolicy.failoverHosts:
+                            if cluster.name in collected_spare_hosts.keys():
+                                #add another element if we have this cluster and more than one spare
+                                collected_spare_hosts[cluster.name].append({'name' : host.name, 'vms' : len(host.vm)})
+                            else:
+                                collected_spare_hosts[cluster.name] = [{ 'name': host.name, 'vms': len(host.vm)}]
+                except Exception as e:
+                    logging.debug(
+                            cluster.name + ": AdmissionControlPolicy not properly configured, bailing out" + str(e))
+                    import traceback
+                    traceback.print_exc()
 
         for clustername in collected_spare_hosts.keys():
             count_vms = 0
             count_failoverhosts = 0
             for pair in collected_spare_hosts[clustername]:
-                count_vms += pair['vms']
+                self.gauge['vcenter_vms_on_failover_hosts'].labels(self.vcenterInfo['hostname'],clustername,pair['name']).set(pair['vms'])
                 count_failoverhosts += 1
-            self.gauge['vcenter_vcenter_vms_on_failover_hosts'].labels(self.vcenterInfo['hostname'],clustername).set(count_vms)
-            # self.gauge['vcenter_vcenter_vms_on_failover_hosts'].labels(self.vcenterInfo['hostname'],clustername,pair['name']).set(count_vms)
-            self.gauge['vcenter_vcenter_failover_host'].labels(self.vcenterInfo['hostname'],clustername).set(count_failoverhosts)
-            self.metric_count += 2
+                self.metric_count += 1
+            self.gauge['vcenter_failover_host'].labels(self.vcenterInfo['hostname'],clustername).set(count_failoverhosts)
+            self.metric_count += 1
+
+        self.gauge['vcenter_prod_cluster'].labels(self.vcenterInfo['hostname']).set(cluster_count)
 
         # Get current session information and check with saved sessions info
         logging.debug('getting api session information')
