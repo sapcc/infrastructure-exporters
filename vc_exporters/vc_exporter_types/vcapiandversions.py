d67086023e09aedf45316087ba0ff3eb5b1dc52e
Tommy Sauer <tommy.sauer@sap.com>
2019-11-04 13:46:03 +0100

    adding logic for querying hv metrics

--- vc_exporters/vc_exporter_types/vcapiandversions.py
+++ vc_exporters/vc_exporter_types/vcapiandversions.py
@@ -2,6 +2,11 @@ import logging
 from vc_exporters.vc_exporter import VCExporter
 from prometheus_client import Gauge
 from pyVmomi import vim
+import math
+from datetime import datetime, timedelta
+import re
+import socket
+import errno
 
 class Vcapiandversions(VCExporter):
     
@@ -11,7 +16,8 @@ class Vcapiandversions(VCExporter):
         self.sessions_dict = {}
         self.host_properties =[
             "summary.config.name", "config.product.version",
-            "config.product.build"
+            "config.product.build", "summary.quickStats.overallMemoryUsage",
+            "hardware.memorySize"
         ]
         self.gauge['vcenter_esx_node_info'] = Gauge('vcenter_esx_node_info',
                                                     'vcenter_esx_node_info',
@@ -46,6 +52,7 @@ class Vcapiandversions(VCExporter):
                                                     'Count of configured failover nodes in the cluster',
                                                     ['hostname', 'cluster'])
 
+
         self.content = self.si.RetrieveContent()
         self.clusters = [cluster for cluster in
                          self.content.viewManager.CreateContainerView(
@@ -57,6 +64,35 @@ class Vcapiandversions(VCExporter):
             type=[vim.HostSystem],
             recursive=True
         )
+
+        #foo we need for perfmgr
+        self.counter_info = {}
+        perf_manager = self.content.perfManager
+        metrics = perf_manager.QueryPerfCounterByLevel(level=2)
+
+        for metric in metrics:
+            full_name = '.'.join([metric.groupInfo.key,
+                                  metric.nameInfo.key,
+                                  metric.rollupType])
+            self.counter_info[full_name] = metric.key
+
+        selected_metrics = self.exporterInfo['hv_metrics']
+        if selected_metrics:
+            self.counter_ids_to_collect = [self.counter_info[i] for i
+                                           in selected_metrics
+                                           if i in self.counter_info]
+        else:
+            self.counter_ids_to_collect = [i.key for i in self.counter_info]
+
+        for counter_id in selected_metrics:
+            vc_gauge = 'vcenter_esxi_' + counter_id.replace('.', '_')
+            self.gauge[vc_gauge] = Gauge(vc_gauge, vc_gauge, [
+                'hostname', 'esxi_host', 'metric_detail' ])
+
+        self.counter_info_keys_list = list(self.counter_info.keys())
+        self.counter_info_keys_underscore = [x.replace('.', '_')
+                                             for x in self.counter_info_keys_list]
+        self.counter_info_values_list = list(self.counter_info.values())
 
     def collect(self):
         region = self.vcenterInfo['hostname'].split('.')[2]
@@ -83,9 +119,10 @@ class Vcapiandversions(VCExporter):
 
         logging.debug('getting version information for each esx host')
 
-        host_data, mors = self.collect_properties(self.si, self.hosts,
+        self.host_data, self.mors = self.collect_properties(self.si, self.hosts,
                                     vim.HostSystem, self.host_properties, True)
-        for host in host_data:
+
+        for host in self.host_data:
             try:
                 logging.debug(host['summary.config.name'] + ": " +
                                 host['config.product.version'])
@@ -154,6 +191,98 @@ class Vcapiandversions(VCExporter):
         self.gauge['vcenter_vcenter_api_active_count'].labels(self.vcenterInfo['hostname']).set(
             len(self.current_sessions)
         )
+
+
+        vch_time = self.si.CurrentTime()
+        start_time = vch_time - \
+            timedelta(seconds=(int(self.exporterInfo['collection_interval'])
+                               + 60))
+        end_time = vch_time - timedelta(seconds=60)
+        perf_manager = self.si.content.perfManager
+
+        queryResult = []
+        queryDict = {}
+
+        for group in self.chuncker(self.host_data, 50):
+            perfQueries = []
+            for item in group:
+                metric_ids = [
+                        vim.PerformanceManager.MetricId(
+                            counterId=i, instance="*") for i
+                            in self.counter_ids_to_collect
+                    ]
+                hv_instance = self.mors[item["obj"]]
+                spec = vim.PerformanceManager.QuerySpec(
+                        maxSample=1,
+                        entity=hv_instance,
+                        metricId=metric_ids,
+                        intervalId=20,
+                        startTime=start_time,
+                        endTime=end_time)
+                perfQueries.append(spec)
+                self.metric_count += 1
+            if len(perfQueries) > 0:
+                queryResult.append(perf_manager.QueryStats(querySpec=perfQueries))
+
+        for group in queryResult:
+            for hv in group:
+                queryDict[hv.entity] = hv
+
+        for item in self.host_data:
+            try:
+                logging.debug('current hv processed - ' +
+                              item["summary.config.name"])
+
+                hv_instance = self.mors[item["obj"]]
+
+                for val in queryDict[hv_instance].value:
+
+                    # send gauges to prometheus exporter: metricname and value with
+                    # labels: vm name, project id, vcenter name, vcneter
+                    # node, instance uuid and metric detail (for instance
+                    # a partition for io or an interface for net metrics)
+                    # - weupdate the gauge only if the value is not -1
+                    # which means the vcenter has no value
+                    if val.value[0] != -1:
+                        if val.id.instance == '':
+                            metric_detail = 'total'
+                        else:
+                            metric_detail = val.id.instance
+
+                        gauge_finder = self.counter_info_values_list.index(val.id.counterId)
+                        gauge_title = self.counter_info_keys_underscore[gauge_finder]
+                        gauge_title = 'vcenter_esxi_' + gauge_title
+                        gauge_title = re.sub('\.', '_', gauge_title)
+                        try:
+                            self.update_gauge(gauge_title,
+                                              self.vcenterInfo['hostname'],
+                                              item["summary.config.name"],
+                                              metric_detail,
+                                              val.value[0])
+                        except socket.error as e:
+                            if e.errno != erno.EPIPE:
+                                raise
+                            logging.info("Brokent pipe updating gauge")
+                        except Exception as e:
+                            logging.info("Unable to update gauge " + str(e))
+
+                logging.debug('==> gauge loop end: %s' % datetime.now())
+                logging.debug("collected data for " + item['summary.config.name'])
+            except KeyError as e:
+                logging.debug("property not defined for hv: " + str(e))
+            except Exception as e:
+                logging.info("couldn't get perf data: " + str(e))
+
+    def chuncker(self, seq, size):
+        return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+    def update_gauge(self, gauge_title, vcenter, esxi_host, metric_detail, metric_val):
+        self.gauge[gauge_title].labels(
+                vcenter,
+                esxi_host,
+                metric_detail).set(metric_val)
+
+
 
     def do_failover_metrics(self):
         collected_spare_hosts = dict()
