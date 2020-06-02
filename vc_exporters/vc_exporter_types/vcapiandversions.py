9e7e643d3d41de25d592071d44a53a32a4edf3d5
Abhishek Kumar <Abhishek.kumar17@sap.com>
2020-06-02 14:22:57 +0530

    consider 256GB VM also as big-VM

--- vc_exporters/vc_exporter_types/vcapiandversions.py
+++ vc_exporters/vc_exporter_types/vcapiandversions.py
@@ -410,7 +410,7 @@ class Vcapiandversions(VCExporter):
             try:
                 if hasattr(vm.summary.config, 'memorySizeMB') and hasattr(vm.runtime, 'powerState'):
                     if vm.summary.config.memorySizeMB and vm.runtime.powerState == 'poweredOn':
-                        if vm.summary.config.memorySizeMB >= 262144:
+                        if vm.summary.config.memorySizeMB >= 231056:
                             host_vms_sum_mb += vm.summary.config.memorySizeMB
             except Exception as e:
                 #for now i want to see it regardless of logging
